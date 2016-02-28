4a19867eb15d68844d24f2ed63d5b907e5c9200d
nmly68it <nmly68it@gmail.com>
2016-02-29 00:02:43 +0700

    first commit

---
+++ src/main/java/com/nmly/springsecurity/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.nmly.springsecurity.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
